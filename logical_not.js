dbf189f0361a5e1ecf61fd349a071bf5a68ee1c1
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-28 22:42:23 +0600

    logical not

---
+++ logical_not.js
@@ -0,0 +1,17 @@
+const isLeader = true;
+// if(isLeader)
+// if(isLeader) {
+//     console.log("Sobai Darao")
+// }
+// else{
+//     console.log("Dhukte dibi na")
+// }
+
+const isPassed = false;
+// if(isPassed === false)
+if(!!isPassed){
+    console.log("tuke biya diya dimu")
+}
+else{
+    console.log("tmi amader noyoner moni")
+}
