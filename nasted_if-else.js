2f4db351e2501d5f58d5df43caa8f6e2da33f6d7
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-28 21:31:51 +0600

    nasted if-else

---
+++ nasted_if-else.js
@@ -0,0 +1,18 @@
+const money = 10;
+
+if (money > 300) {
+    console.log("Bro you are posh")
+}
+else {
+    if( money > 200) {
+        console.log("Tui gorib o na boro lok o na")
+    }
+    else {
+        if (money > 50) {
+            console.log("tui shala taka kama")
+        }
+        else{
+            console.log("tui shala gorib")
+        }
+    }
+}
