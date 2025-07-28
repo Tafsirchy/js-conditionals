fbdcb1f3b7ad0bf5bd63374ab77b6539de29a838
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-28 20:27:15 +0600

    if-else statement

---
+++ if.js
@@ -0,0 +1,27 @@
+/**if(condition){
+ * code to be executed
+} */
+
+if(3 < 10){
+    console.log ("I have a smaller value")
+}
+
+var biriyaniPrice = 500;
+
+if(biriyaniPrice < 600){
+    console.log("mama give me some biriyani")
+}
+/**if(condition){
+ * code to be executed if condition is true
+}
+else{
+ * code to be executed if condition is false} */
+
+var weight = 20;
+
+if(weight <= 20){
+    console.log("I will carry")
+}
+else{
+    console.log("I will take a rickshaw")
+}
--- relational.js
+++ relational.js
@@ -1,6 +1,6 @@
 var math = 75.25;
 var biology = 65;
-var chemistry = 80;
+var chemistry = 80; 
 var physics = 35.45;
 var bangla = 99.50;
 
