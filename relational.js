a79a27872e15e1088d46e6c5318d452fb08eb912
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-28 12:42:35 +0600

    operations

---
+++ relational.js
@@ -0,0 +1,10 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+sum = (math + biology + chemistry + physics + bangla);
+avg = sum / 5;
+
+console.log(avg.toFixed(2));
