8f20f31fbde337ffe7cc06d5607ba3bbc55a5872
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-28 20:08:15 +0600

    conditionals

--- relational.js
+++ relational.js
@@ -8,3 +8,21 @@ sum = (math + biology + chemistry + physics + bangla);
 avg = sum / 5;
 
 console.log(avg.toFixed(2));
+
+/**
+ * COMPARISON
+ * 1. greater than (>)
+ * 2. less than (<)
+ * 3. greater than or equal to (>=)
+ * 4. less than or equal to (<=)
+ * 5. equal to (==)
+ * 6. not equal to (!=)
+ * 
+ * ------------------------------------
+ * 7. logical and (&&)
+ * 8. logical or (||)
+ * 9. logical not (!)
+ * 10. strict equal (===)
+ * 11. strict not equal (!==)
+ * 
+ */
