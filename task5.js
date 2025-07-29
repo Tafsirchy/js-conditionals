0d3058a8025d69158ef048e062f36ae803f97f92
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-29 20:42:16 +0600

    task5

---
+++ task5.js
@@ -0,0 +1,19 @@
+let num1 = 25;
+let num2 = 10;
+
+let result ;
+
+// if(num1 > num2){
+//     result = num1 * 2
+//     console.log(result)
+// }
+// else{
+//     result = num1 + num2;
+//     console.log(result)
+// }
+
+// ternary operator
+
+(num1 > num2) ? result = num1 * 2 : result = num1 + num2;
+
+console.log(result)
