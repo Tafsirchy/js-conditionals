5a2ad4529393d738a37f126bf4e626011d98135d
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-29 19:47:41 +0600

    task2

---
+++ task2.js
@@ -0,0 +1,17 @@
+let weight = 70;
+let height = 1.67;
+
+let bmi = weight / (height * height);
+
+if (bmi < 18.5){
+    console.log("You are Underweight");
+}
+else if(bmi >= 18.5 && bmi <=24.9){
+    console.log("You are Normal");
+}
+else if(bmi >= 25 && bmi <= 29.9){
+    console.log("You are Overweight");
+}
+else{
+    console.log("You are Obese");
+}
